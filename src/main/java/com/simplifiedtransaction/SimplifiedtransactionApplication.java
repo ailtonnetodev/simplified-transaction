a943f23909d93b7cc1e29f445a47dc8ddba28fd5
ailtonnetodev <ailtonnetto8319@gmail.com>
2024-09-18 12:28:59 -0300

    feat: initial config

---
+++ src/main/java/com/simplifiedtransaction/SimplifiedtransactionApplication.java
@@ -0,0 +1,13 @@
+package com.simplifiedtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedtransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedtransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifiedtransaction/SimplifiedtransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifiedtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedtransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
