d96ce7e9355ba865110d18d7a18a9cb0a0d43a5c
ailtonnetodev <ailtonnetto8319@gmail.com>
2024-09-19 19:44:49 -0300

    feat: Infra Config

---
+++ src/main/java/com/simplifiedtransaction/infra/AppConfig.java
@@ -0,0 +1,13 @@
+package com.simplifiedtransaction.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
