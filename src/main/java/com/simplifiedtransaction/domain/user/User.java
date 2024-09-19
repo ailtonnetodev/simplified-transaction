79b9c5c57043eed35bf7781db50a90fbd25edcbd
ailtonnetodev <ailtonnetto8319@gmail.com>
2024-09-19 19:40:26 -0300

    feat: create User Model

--- src/main/java/com/simplifiedtransaction/domain/user/User.java
+++ src/main/java/com/simplifiedtransaction/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.simplifiedtransaction.domain.user;
 
 import com.simplifiedtransaction.domain.UserType;
+import com.simplifiedtransaction.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -38,4 +37,16 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.email = data.email();
+        this.document = data.document();
+        this.password = data.password();
+
+    }
 }
+
